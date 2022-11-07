80504c69c850c21a33b68c6267597e8e40ffbb36
jolietran525 <117689422+jolietran525@users.noreply.github.com>
2022-11-07 14:29:27 -0800

    Delete hw2-q4.sql

--- hw2-q4.sql
+++
@@ -1,6 +0,0 @@
-## Question 4 (12 rows)
-SELECT DISTINCT c.name AS name
-FROM FLIGHTS f, CARRIERS c
-WHERE f.carrier_id = c.cid
-GROUP BY c.name, f.month_id, f.day_of_month
-HAVING COUNT(f.fid) > 1000;
