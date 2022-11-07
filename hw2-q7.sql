05eee13b2854d694e1527bc8c064b970a2bebbd5
jolietran525 <117689422+jolietran525@users.noreply.github.com>
2022-11-07 14:30:03 -0800

    Delete hw2-q7.sql

--- hw2-q7.sql
+++
@@ -1,9 +0,0 @@
-## Question 7 (1 row)
-SELECT SUM(f.capacity) AS capacity
-FROM FLIGHTS f
-WHERE f.month_id = 7 AND
-      f.day_of_month = 10 AND
-      ((f.origin_city = 'Seattle WA' AND
-      f.dest_city = 'San Francisco CA') OR
-      (f.origin_city = 'San Francisco CA' AND
-      f.dest_city = 'Seattle WA'));
